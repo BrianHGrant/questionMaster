7b57cac1dd04a2c58af7db61b8764a2ec603346e
BrianHGrant <bhgrant8@gmail.com>
2016-08-19 11:03:33 -0700

    create basic landing page styling and display of question of day and all questions list

--- app/models/question.js
+++ app/models/question.js
@@ -1,8 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  text: DS.attr();
-  author: DS.attr();
-  note: DS.attr();
-  timestamp: DS.attr();
+  "text": DS.attr(),
+  "author": DS.attr(),
+  "note": DS.attr(),
+  "timestamp": DS.attr()
 });
