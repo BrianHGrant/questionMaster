72cbcd987e7617bebe152dc32725ee6f3a137e9b
BrianHGrant <bhgrant8@gmail.com>
2016-08-28 10:17:59 -0700

    update file not saved prior to commit

--- app/routes/avatar.js
+++ app/routes/avatar.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('user')
+    return this.store.findAll('user');
   },
   userAuth: Ember.inject.service(),
 
